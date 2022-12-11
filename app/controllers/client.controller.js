2eb1466b82be3d7b909601d2d5ccd362b661fc20
Wheelzzz <kaseywheeler@gmail.com>
2022-12-11 10:02:54 -0600

    ddd

--- app/controllers/client.controller.js
+++ app/controllers/client.controller.js
@@ -5,10 +5,8 @@ const Op = db.Sequelize.Op;
 // Create and Save a new Client
 exports.create = (req, res) => {
   // Validate request
-  if (!req.body.title) {
-    res.status(400).send({message: "Content can not be empty!"});
-    return;
-  }
+  if (!req.body.firstName) {res.status(400).send({message: "Content can not be empty!"});
+    return; }
 
   // Create a Client
   const client = {
